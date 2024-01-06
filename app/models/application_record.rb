c37499b68fa6280b10a76365c7c1c8997a3395fd
K234k <kaitoueno2@gmail.com>
2024-01-06 23:25:39 +0900

    ビューの作成

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,12 +1,6 @@
-class User < ApplicationRecord
-  validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[0-9])(?=.*[a-zA-Z])/ }
-  validates :password_confirmation, presence: true
-  # 他のバリデーションも追加する
+  class ApplicationRecord < ActiveRecord::Base
+    primary_abstract_class
+  end
+  
 
-  # 本人情報確認のバリデーション
-  validates :full_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :full_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
-end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        
+         validates :nickname, presence: true
+         validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+         validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[0-9])(?=.*[a-zA-Z])/ }
+         validates :password_confirmation, presence: true
+         validates :last_name, presence: true
+         validates :first_name, presence: true
+         validates :last_name_kana, presence: true
+         validates :first_name_kana, presence: true
+         validates :birthday, presence: true
 end
