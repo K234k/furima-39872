b1913f458b01542a594268179c9c50b92a5f42bc
K234k <kaitoueno2@gmail.com>
2024-01-10 19:30:27 +0900

    異常系テストを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー－一-龠々]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー－一-龠々]+\z/ }
   validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -57,6 +57,18 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
 
+      it '重複したメールアドレスは登録できない' do
+        @user.save
+        duplicate_user = @user.dup
+        duplicate_user.email = @user.email.upcase 
+        expect(duplicate_user.valid?).to be_falsey
+      end
+    
+      it 'メールアドレスに@を含まない場合は登録できない' do
+        @user.email = 'invalid_email_example.com'
+        expect(@user.valid?).to be_falsey
+      end
+
       it 'パスワードと確認用パスワードが一致しないと登録できない' do
         @user.password_confirmation = 'ab987b'
         @user.valid?
@@ -74,6 +86,22 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
+
+      it '英字のみのパスワードでは登録できない' do
+        @user.password = 'onlyletters'
+        expect(@user.valid?).to be_falsey
+      end
+
+      it '数字のみのパスワードでは登録できない' do
+        @user.password = '1234567890'
+        expect(@user.valid?).to be_falsey
+      end
+    
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'パスワード１２３'
+        expect(@user.valid?).to be_falsey
+      end
+
       it '名字が全角（漢字・ひらがな・カタカナ）でないと登録できない' do
         @user.last_name = 'furima'
         @user.valid?
