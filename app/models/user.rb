349a292e5e597ab597d1d5168685998b8a3d6086
K234k <kaitoueno2@gmail.com>
2024-01-10 16:26:04 +0900

    テストコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
         
          validates :nickname, presence: true
          validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-         validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[0-9])(?=.*[a-zA-Z])/ }
          validates :password_confirmation, presence: true
          validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
          validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
