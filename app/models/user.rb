e87b5edc32f873e4a12bc708569cab7188755317
K234k <kaitoueno2@gmail.com>
2024-01-10 18:05:18 +0900

    RuboCop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-        
-         validates :nickname, presence: true
-         validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー－一-龠々]+\z/ }
-         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー－一-龠々]+\z/ }
-         validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-         validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-         validates :birthday, presence: true
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー－一-龠々]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー－一-龠々]+\z/ }
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.name}
-    email                 {Faker::Internet.email}
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.email }
     password              { '1a' + Faker::Internet.password(min_length: 4) }
-    password_confirmation {password}
-    last_name             {'振真'}
-    first_name            {'西都'}
-    last_name_kana        {'フリマ'}
-    first_name_kana       {'サイト'}
-   birthday               {Faker::Date.birthday}
+    password_confirmation { password }
+    last_name             { '振真' }
+    first_name            { '西都' }
+    last_name_kana        { 'フリマ' }
+    first_name_kana       { 'サイト' }
+    birthday { Faker::Date.birthday }
   end
 end
