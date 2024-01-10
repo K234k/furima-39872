25cb6480cad42d09d5541362265c6045d9e28ca0
K234k <kaitoueno2@gmail.com>
2024-01-10 16:34:07 +0900

    バリテーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,8 @@ class User < ApplicationRecord
         
          validates :nickname, presence: true
          validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-         validates :password_confirmation, presence: true
-         validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-         validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :last_name, format: { with: /\A[ぁ-んァ-ヶー－]+\z/ }
+         validates :first_name, format: { with: /\A[ぁ-んァ-ヶー－]+\z/ } 
          validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :birthday, presence: true
