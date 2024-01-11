59fc4733aff10f1d42cbd3ebfcf313cfaff216da
K234k <kaitoueno2@gmail.com>
2024-01-12 02:08:19 +0900

    Active Storageインストール

--- db/migrate/20240111150759_create_items.rb
+++ db/migrate/20240111150759_create_items.rb
@@ -1,14 +1,15 @@
-class CreateProducts < ActiveRecord::Migration[7.0]
+class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      t.string :name
-      t.text :description
-      t.string :category
-      t.string :condition
-      t.string :shipping_fee_burden
-      t.string :shipping_from
-      t.string :shipping_days
-      t.integer :price
+      t.references  :user,              foreign_key: true, null: false
+      t.string      :name,              null: false
+      t.text        :description,       null: false
+      t.integer     :category_id,       null: false
+      t.integer     :item_status_id,    null: false
+      t.integer     :shipping_cost_id,  null: false
+      t.integer     :prefecture_id,     null: false
+      t.integer     :shipping_date_id,  null: false
+      t.integer     :price,             null: false
 
       t.timestamps
     end
