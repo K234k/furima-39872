47f6ba9869411480dfb1bc71e7971a9b6d92f68d
K234k <kaitoueno2@gmail.com>
2024-01-12 23:45:57 +0900

    ActiveHashの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,5 +6,27 @@ class Item < ApplicationRecord
 
   # ActiveHashのアソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category  # ActiveHashモデルのカテゴリーとの関連
+  belongs_to :category  
+  belongs_to :item_status  
+  belongs_to :shipping_cost
+  belongs_to :prefecture  
+  belongs_to :shipping_date  
+
 end
+
+ # バリデーション
+ validates :name, presence: true, length: { maximum: 40 }
+ validates :description, presence: true, length: { maximum: 1000 }
+ validates :category, presence: true
+ validates :item_status, presence: true
+ validates :shipping_cost, presence: true
+ validates :prefecture, presence: true
+ validates :shipping_date, presence: true
+ validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+ validate :validate_image_attachment  # 商品画像の添付が必須
+
+ def validate_image_attachment
+  errors.add(:base, '商品画像を添付してください') unless image.attached?
+end
+
