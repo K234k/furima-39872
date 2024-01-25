a0e391f2bedbf00c3c65996c3eed6828c161f2ff
K234k <kaitoueno2@gmail.com>
2024-01-25 22:14:42 +0900

    rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,14 +38,12 @@ class ItemsController < ApplicationController
 
   private
 
-
   def set_item
     @item = Item.find(params[:id])
-    end
-   end
-
-  def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id,
-                                 :shipping_date_id, :price).merge(user_id: current_user.id)
   end
+end
+
+def item_params
+  params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id,
+                               :shipping_date_id, :price).merge(user_id: current_user.id)
 end
