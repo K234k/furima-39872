b815fa91326c51d846f8c0d826a5c3281b434c3e
K234k <kaitoueno2@gmail.com>
2024-02-07 21:10:02 +0900

    テストコード記述

---
+++ spec/factories/order_forms.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :order_form do
+    user_id { Faker::Number.non_zero_digit }
+    item_id { Faker::Number.non_zero_digit }
+    postcode { "#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" }
+    prefecture_id { Faker::Number.between(from: 2, to: 47) } # 1は「---」を避ける
+    city { Faker::Address.city }
+    block { Faker::Address.street_address }
+    building { Faker::Address.secondary_address }
+    phone_number { Faker::Number.leading_zero_number(digits: 10) }
+    token { Faker::Internet.password(min_length: 20, max_length: 30) }
+  end
+end
