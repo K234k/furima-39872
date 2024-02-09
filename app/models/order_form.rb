b86c0f7b5e25779084d6e24ca357e77e420cd514
K234k <kaitoueno2@gmail.com>
2024-02-10 00:14:41 +0900

    バリテーション修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -6,7 +6,7 @@ class OrderForm
     validates :user_id
     validates :item_id
     validates :postcode, format: { with: /\A\d{3}-\d{4}\z/, message: 'input correctly' }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :block
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
